7b8729beac2f07952879fd362aefbf26edfb2500
hyejeong0807 <hyejeong12311@gmail.com>
2023-03-15 13:02:47 +0900

    App test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          /learn react/i
+          learn react
         </a>
       </header>
     </div>
